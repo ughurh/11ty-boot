13b8ac550515dde46e2a105049eb3f7594894e6c
ughurh <ughur.huseynov@gmail.com>
2021-09-30 09:52:04 +0400

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy('src/assets');
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+      layouts: '_layouts',
+    },
+    templateFormats: [
+      'html',
+      'md',
+      'njk',
+    ],
+    // pathPrefix: '/YOUR_REPO_NAME_HERE/', // omit this line if using custom domain
+  };
+};
